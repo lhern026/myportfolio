1d2c53e356d0984214b037f46fb80a35a9844dd5
luis hernandez <lhern026@gmail.com>
2023-01-15 22:39:17 -0800

    implemented tailwind

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,8 +2,12 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
+  plugins: [
+    "gatsby-plugin-postcss",
+    // ...
+  ],
   siteMetadata: {
     siteUrl: `https://www.yourdomain.tld`,
   },
   plugins: [],
-}
+};
